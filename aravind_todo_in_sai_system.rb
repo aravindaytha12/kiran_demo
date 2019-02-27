84d1e6bb43671bad35ecb242d7c1fde23b6e79dd
Aravind Aytha 12 <35966725+aravindaytha12@users.noreply.github.com>
2019-02-27 10:45:56 +0530

    Create aravind_todo_in_sai_system.rb
    Aravind todo changes in sai vardhans system

---
+++ aravind_todo_in_sai_system.rb
@@ -0,0 +1,91 @@
+dont touch
+C:\Users\sturimella\gannett-gics\trunk
+
+trail path:
+C:\Users\sturimella\Documents\gannett-gics\trunk
+
+#=======================================================================================
+
+https://www.zdlc.org/fs/s/aD3IFnLO5PafMcJ  
+please use teh below jar
+ zdlc1234
+#=======================================================================================
+
+
+1) under ==> Documents
+Extracter_LEGACY_21_FEB.jar
+#Under Administrator: Command Prompt
+C:\Users\sturimella\Documents> java -jar Extracter_LEGACY_21_FEB.jar
+C:\Users\sturimella\Documents> java -jar Extracter_LEGACY_21_FEB_2.jar
+#=======================================================================================
+
+2) app/controllers/test
+app/controllers/test/accounts_controller.rb
+
+#Main file Path ==> C:\Users\sturimella\Documents\gannett-gics\trunk\app\controllers\accounts_controller.rb
+
+def ==> 114
+	:: STARTED  ==> 113
+	:: ENDED  ==> 113
+end
+#=======================================================================================
+
+
+
+3) def save_end_call
+end
+
+
+4) def save_end_call
+  # This method is called from end call tab.  If the flag is set to true it 
+  # will s$LOG.debug("#{DateTime.now.strftime('%Q')} :: #{DateTime.now.strftime('%Q')} :: #{class_method} :: ENDED")
+end emails or else it will delete the record in AMSE and AMCR
+
+#1755 line number
+
+
+def load_resolution_options_action
+end
+# Append +TV depending on the resolution type. 
+
+#=======================================================================================
+
+5) def aaaa
+	if condition
+	else	
+	end
+end
+#will send email
+
+6) def aaaa
+	if condition
+	else	
+	end
+end
+#will s$LOG.debug("#{DateTime.now.strftime('%Q')} :: #{DateTime.now.strftime('%Q')} :: #{class_method} :: ENDED")
+end email
+#=======================================================================================
+
+7) 2nd new Jar file #-- 21st feb 2019
+https://www.zdlc.org/fs/s/aD3IFnLO5PafMcJ
+zdlc1234
+
+Again at feb 22nd:
+https://www.zdlc.org/fs/s/euaO9GvHwHvYm9g
+zdlc1234
+#=======================================================================================
+8) how this self.class.name witll gets reflects
+class_method = "#{self.class.name} :: #{__method__}"
+#=======================================================================================
+22-02-2019:
+
+1) my controller file changes
+
+accounts/start_controller.rb
+accounts/transfer_controller.rb
+accounts/web_to_cash_controller.rb
+accounts_controller.rb
+
+
+
+
